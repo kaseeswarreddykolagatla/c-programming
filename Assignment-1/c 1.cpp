e41ec6c774854ff5045558b55b458c454ec16942
kaseeswarreddykolagatla <123532622+kaseeswarreddykolagatla@users.noreply.github.com>
2023-03-29 13:46:05 +0530

    Add files via upload
    Signed-off-by: kaseeswarreddykolagatla <123532622+kaseeswarreddykolagatla@users.noreply.github.com>

---
+++ Assignment-1/c 1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    if(num > 0) {
+        printf("%d is positive.", num);
+    }
+    else if(num < 0) {
+        printf("%d is negative.", num);
+    }
+    else {
+        printf("The number is zero.");
+    }
+
+    return 0;
+}
